4ff938794e49650280c86e087ee61472f84d868a
Jair Gomez <jairherber505@gmail.com>
2023-11-13 11:25:02 -0800

    search.tsx is reading the value.terget and updating the url

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,7 +1,6 @@
 import RevenueChart from "../../ui/dashboard/revenue-chart";
 import LatestInvoices from "../../ui/dashboard/latest-invoices";
 import { lusitana } from "../../ui/fonts";
-
 import { Suspense } from "react";
 import {
   RevenueChartSkeleton,

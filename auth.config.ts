e787124c6731ce96d25d57f0292a9627ec302d7b
Jair Gomez <jairherber505@gmail.com>
2023-11-27 11:04:32 -0800

    it was an environment variable problem with vercel, login/logout works now

--- auth.config.ts
+++ auth.config.ts
@@ -1,41 +1,21 @@
-// import NextAuth from "next-auth";
-// import { authConfig } from "./auth.config";
-// import Credentials from "next-auth/providers/credentials";
-// import { z } from "zod";
-// import { sql } from "@vercel/postgres";
-// import type { User } from "@/app/lib/definitions";
-// import bcrypt from "bcrypt";
-// import { log } from "console";
+import type { NextAuthConfig } from "next-auth";
 
-// async function getUser(email: string): Promise<User | undefined> {
-//   try {
-//     const user = await sql<User>`SELECT * FROM users WHERE email=${email}`;
-//     return user.rows[0];
-//   } catch (e) {
-//     console.log("Failed to fetch user", e);
-//     throw new Error("Failed to fetch user.");
-//   }
-// }
-
-// export const { auth, signIn, signOut } = NextAuth({
-//   ...authConfig,
-//   providers: [
-//     Credentials({
-//       async authorize(credentials) {
-//         const parsedCredentials = z
-//           .object({ email: z.string().email(), password: z.string().min(6) })
-//           .safeParse(credentials);
-
-//         if (parsedCredentials.success) {
-//           const { email, password } = parsedCredentials.data;
-//           const user = await getUser(email);
-//           if (!user) return null;
-//           const passwordsMatch = await bcrypt.compare(password, user.password);
-//           if (passwordsMatch) return user;
-//         }
-//         console.log("Invalid Credentials");
-//         return null;
-//       },
-//     }),
-//   ],
-// });
+export const authConfig = {
+  pages: {
+    signIn: "/login",
+  },
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn = !!auth?.user;
+      const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
+      if (isOnDashboard) {
+        if (isLoggedIn) return true;
+        return false;
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL("/dashboard", nextUrl));
+      }
+      return true;
+    },
+  },
+  providers: [],
+} satisfies NextAuthConfig;
